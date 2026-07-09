ef72270524bad5da930024c31f2527e1400a6433
Ayush Rai <ayushrai9644@gmail.com>
2026-07-09 17:14:38 +0530

    Fix API dev warning

--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -1,6 +1,12 @@
 import app from './app.js';
 import { PORT } from './config/env.js';
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Dynamic Pricing API running on port ${PORT}`);
+});
+
+process.on('SIGINT', () => {
+  server.close(() => {
+    process.exit(0);
+  });
 });
